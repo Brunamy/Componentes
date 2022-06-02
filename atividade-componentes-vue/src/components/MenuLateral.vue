73dc552b80a5c9649f3a6f12dac76130a084b731
Brunamy <87535917+Brunamy@users.noreply.github.com>
2022-06-02 10:51:13 -0300

    Add files via upload

---
+++ atividade-componentes-vue/src/components/MenuLateral.vue
@@ -0,0 +1,15 @@
+<script>
+export default {};
+</script>
+<template>
+  <aside id="sidenav">
+    <ul>
+      <li>Filmes 1</li>
+      <li>Filmes 2</li>
+      <li>Filmes 3</li>
+      <li>Filmes 4</li>
+      <li>Filmes 5</li>
+    </ul>
+  </aside>
+</template>
+<style></style>
